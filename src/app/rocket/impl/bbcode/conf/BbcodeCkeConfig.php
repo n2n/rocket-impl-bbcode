1eb6f739b26e08e5d0098e4f32f2c980d4a30d7a
andreasvonburg <a@von-burg.net>
2021-01-05 11:12:35 +0100

    rocket 3.0 implementations

--- src/app/rocket/impl/bbcode/conf/BbcodeCkeConfig.php
+++ src/app/rocket/impl/bbcode/conf/BbcodeCkeConfig.php
@@ -4,10 +4,10 @@ namespace rocket\impl\bbcode\conf;
 use rocket\ei\util\Eiu;
 use n2n\util\type\attrs\DataSet;
 use n2n\web\dispatch\mag\MagCollection;
-use rocket\impl\ei\component\prop\adapter\config\ConfigAdaption;
+use rocket\impl\ei\component\prop\adapter\config\PropConfigAdaption;
 use rocket\impl\ei\component\prop\string\cke\conf\CkeConfig;
 
-class BbcodeCkeConfig extends ConfigAdaption {
+class BbcodeCkeConfig extends PropConfigAdaption {
 	
 	public function mag(Eiu $eiu, DataSet $dataSet, MagCollection $magCollection) {
 		$magCollection->removeMagByPropertyName(CkeConfig::ATTR_TABLES_SUPPORTED_KEY);
